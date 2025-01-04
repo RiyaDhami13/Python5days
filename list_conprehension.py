d425ed8234e147bd706c92d0642dc3922db76803
RiyaDhami13 <132061713+RiyaDhami13@users.noreply.github.com>
2025-01-04 20:45:59 +0545

    Update list_conprehension.py

--- list_conprehension.py
+++ list_conprehension.py
@@ -9,3 +9,11 @@ jean = '24601'
 flash = ['jay garrick', 'barry allen', 'wally west', 'bart allen']
 
 valjean = 24601
+
+
+# Create a 5 x 5 matrix using a list of lists: matrix
+matrix = [[col for col in range(5)] for row in range(5)]
+
+# Print the matrix
+for row in matrix:
+    print(row)
